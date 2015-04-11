d5a737024ddc773b5c8ecd7f5e7e93167c183730
andregarkin <andregarkin@gmail.com>
2015-04-11 18:03:58 +0300

    add Database class, also add Banners list.

--- banners/list.php
+++ banners/list.php
@@ -1,5 +1,6 @@
 <?php
 include ('../app/classes/Session.php');
+include ('../app/classes/Database.php');
 
 $objSession = new Session();
 $objSession->start();
@@ -9,11 +10,6 @@ $objSession->start();
 // plug the config
 include ('../app/config.php'); 
 
-
-// connect to DB
-$dbObject = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
-$dbObject->exec('SET CHARACTER SET utf8');  
-
     
 $objSession->printnow();
 
@@ -21,10 +17,20 @@ $objSession->defineStatus();
 
     
 if (false == LOGGED) {
+  // go home
   header("Location: http://" . SERVER_NAME . SUBFOLDER_PATH);
 }
 
 if (LOGGED) {
+  // show banners list 
+  
+  // connect to DB
+  //$dbObject = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+  //$dbObject->exec('SET CHARACTER SET utf8');  
+  
+  $pdo = Database::connect();
+  $sql = 'SELECT `id`, `title`, `content`, `option_display` FROM `tmcms_tbanners` ORDER BY `id` ASC';
+  
   include ('../tpl/banners_list.tpl.php');  
 }
       
